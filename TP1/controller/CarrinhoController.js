940856b0f040672771d161fcd40c4fccd1d38457
GuiNo12 <guilherme.nogueiracoelho@live.com>
2021-11-30 11:12:46 -0300

    Correção do Carrinho de Compras
    Agora é possível remover itens do carrinho de compras.

--- TP1/controller/CarrinhoController.js
+++ TP1/controller/CarrinhoController.js
@@ -1,39 +1,41 @@
-class CarrinhoController{
-    
-    constructor(){      
+/*
+    Módulo que controla o carrinho de compras
+*/
+class CarrinhoController {
+
+    constructor() {
         this.carrinho = []
-        this.precoTotal = 0        
+        this.precoTotal = 0
     }
 
-    addProdutoCarrinho(nome, preco, quantidade)  {
+    addProdutoCarrinho(nome, preco, quantidade) {
         var item = {
             nome: nome,
             quantidade: quantidade,
             preco: preco
         }
-
-        this.carrinho.push(item)
-        this.precoTotal += preco        
+        if (quantidade != 0) {
+            this.carrinho.push(item)
+            this.precoTotal += preco * quantidade            
+        }
     }
-    
-    removeProdutoCarrinho(nome, preco, quantidade){
+
+    removeProdutoCarrinho(nome) {
         var item = {
-            nome: nome,
-            quantidade: quantidade,
-            preco: preco
+            nome: nome
         }
         var pos = 0
-        for (let x of carrinho){                               
-            if(x.nome == item.nome){                    
-                let removido = carrinho.splice(pos, 1)
-                console.log(pos)                    
-                this.precoTotal -= preco
-            }                  
-            pos = pos + 1               
-        }      
+
+        for (let x of this.carrinho) { //Para cada item no carrinho                     
+            if (x.nome == item.nome) { //Verifica se o nome dele é igual ao nome do item a ser removido                   
+                let removido = this.carrinho.splice(pos, 1)                
+                this.precoTotal -= x.preco * x.quantidade
+            }
+            pos = pos + 1
+        }
     }
 
-    esvaziaCarrinho(){
+    esvaziaCarrinho() {
         this.carrinho = []
         this.precoTotal = 0
     }
